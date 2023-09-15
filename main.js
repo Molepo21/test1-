c40e47f8fe0be7c9540fee66fb57018d0ecf34e6
Molepo21 <kgamakigift@gmail.com>
2023-09-15 14:07:27 +0200

    Added comments to every line of code

--- main.js
+++ main.js
@@ -10,28 +10,40 @@ let listOfObjects =
 //function that deletes an object by Id
 
 function deleteById(id) {
+    //finding the index of the object with the matching ID
     const index = listOfObjects.findIndex(obj => obj.id === id);
+    //checking if the object with the matching ID was found
     if (index !== -1) {
+        //used splice to remove the object at the found index
         listOfObjects.splice(index, 1);
     }
 }
-deleteById(3)
+//Delete an object with ID 1
+deleteById(1)
+//The object with ID 1 has been removed
 console.log(listOfObjects);
 
 
-//function that removes numbers form a string
+//function that removes all the numbers form aw word in an array
 
 function removeNumber(arr) {
+    //Declare numbers for the unwanted numbers in the words
     const numbers = "0123456789";
+    //Declare results to an empty array to store the words without numbers
     const result = [];
-
+//created a for of loop to iterate through every word in the array
     for (const word of arr) {
+        //Declared newWord to an empty string to store words without numbers
         let newWord = "";
-        for (const chars of word) {
-            if (numbers.indexOf(chars) === -1) {
+        //created a for of loop to iterate through every character in the array
+        for (const chars of word){
+            //created an if statement to check if a character is a number or not
+            if (numbers.indexOf(chars) === -1){
+                //Added char to newWord to return words without numbers
                 newWord += chars;
             }
         }
+        //pushed newWord to the empty result array to have an array with words without numbers
         result.push(newWord);
     }
 
@@ -40,7 +52,10 @@ function removeNumber(arr) {
 //array of names created
 
 const arr = ["Gift1","Jack2","Mark3","Moral4","Emily5"]
+//Defined newArray and assing it to removeNumber(arr) in order to have a new array with words without numbers
 const newArray = removeNumber(arr);
 
+//console log newArray to display the list of names without numbers
 console.log(newArray)
+
 
